158fe2000b6cf2f76313f97e27d0bd0cb329a2c1
canaleija <robanac@gmail.com>
2024-09-19 10:43:51 -0600

    ejemplo de pow

---
+++ ejemplopow.c
@@ -0,0 +1,17 @@
+#include <math.h>
+#include <stdio.h>
+
+int main()
+{
+    
+    double x,y,z;
+
+    x=2.0;
+    y=3.0;
+    z=pow(x,y);
+
+
+    printf("%lf to the power of %lf is %lf\n", x, y, z);
+
+    return 0;
+}
--- prueba.c
+++ prueba.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
+#include <math.h>
 
 int main()
 {
     int y = 12;
     int x = 80;
     int *aux = &x;
+    
     
 
     printf("%p\n",aux);
