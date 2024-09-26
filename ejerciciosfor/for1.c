7317186feda0b2e9219fee1b28694018c65f8817
canaleija <robanac@gmail.com>
2024-09-26 11:00:30 -0600

    for basico

---
+++ ejerciciosfor/for1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    int x = 0, y;
+    for(y = 5;x<5;){
+        printf("Hola %d\n",x);
+        x = x + 1;
+    }
+        
+
+    /* code */
+    return 0;
+}
